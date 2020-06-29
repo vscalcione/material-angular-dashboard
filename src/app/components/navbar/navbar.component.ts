e9cd6afc452b057ac23b33d8ad208fece87967ba
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-06-29 23:53:26 +0200

    Add navbar, footer, sidebar components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { ROUTES } from '../sidebar/sidebar.component';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private listTitles: any[];
+  location: Location;
+  mobileMenuVisible: any = 0;
+  private toggleButton: any;
+  private sidebarVisible: boolean;
 
-  ngOnInit(): void {
+  constructor(location: Location, private element: ElementRef, private router: Router) {
+    this.location = location;
+    this.sidebarVisible = false;
   }
 
+  ngOnInit() {
+    this.listTitles = Routes.filter(listTitles => this.listTitles);
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+    this.router.events.subscribe((event) => {
+      this.sidebarClose();
+      var $layer: any = document.getElementsByClassName('close-layer')[0];
+      if ($layer) {
+        $layer.remove();
+        this.mobileMenuVisible = 0;
+      }
+    });
+  }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    body.classList.add('nav-open');
+    this.sidebarVisible = true;
+  };
+
+  sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+  };
+
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    var $toggle = document.getElementsByClassName('navbar-toggler')[0];
+    this.sidebarVisible === false ? this.sidebarOpen() : this.sidebarClose();
+    const body = document.getElementsByTagName('body')[0];
+    if (this.mobileMenuVisible == 1) {
+      // $('html').removeClass('nav-open');
+      body.classList.remove('nav-open');
+      if ($layer) {
+        $layer.remove();
+      }
+      setTimeout(function () {
+        $toggle.classList.remove('toggled');
+      }, 400);
+
+      this.mobileMenuVisible = 0;
+    } else {
+      setTimeout(function () {
+        $toggle.classList.add('toggled');
+      }, 430);
+
+      var $layer = document.createElement('div');
+      $layer.setAttribute('class', 'close-layer');
+
+
+      if (body.querySelectorAll('.main-panel')) {
+        document.getElementsByClassName('main-panel')[0].appendChild($layer);
+      } else if (body.classList.contains('off-canvas-sidebar')) {
+        document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
+      }
+
+      setTimeout(function () {
+        $layer.classList.add('visible');
+      }, 100);
+
+      $layer.onclick = function () { //asign a function
+        body.classList.remove('nav-open');
+        this.mobile_menu_visible = 0;
+        $layer.classList.remove('visible');
+        setTimeout(function () {
+          $layer.remove();
+          $toggle.classList.remove('toggled');
+        }, 400);
+      }.bind(this);
+
+      body.classList.add('nav-open');
+      this.mobileMenuVisible = 1;
+
+    }
+  };
+
+  getTitle() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+
+    for (var item = 0; item < this.listTitles.length; item++) {
+      if (this.listTitles[item].path === titlee) {
+        return this.listTitles[item].title;
+      }
+    }
+    return 'Dashboard';
+  }
 }
