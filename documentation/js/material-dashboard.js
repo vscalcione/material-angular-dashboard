c1ba52425fe8b30d2b4e5cbe02ac7ae6f7fb2bed
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-02 23:56:37 +0200

    Update pages

--- documentation/js/material-dashboard.js
+++ documentation/js/material-dashboard.js
@@ -1,20 +1,3 @@
-/*!
-
- =========================================================
- * Material Dashboard Angular 2 - V1.2.0
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
- * Copyright 2017 Creative Tim (https://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
-
-
 var searchVisible = 0;
 var transparent = true;
 
